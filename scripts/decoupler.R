fd9da9db86ac6f2b4ba72e70b2d6f081e4e3b6c7
Victor Paton <victor.paton@outlook.com>
2022-10-18 14:34:14 +0200

    added heatmap plotter

--- scripts/decoupler.R
+++ scripts/decoupler.R
@@ -10,7 +10,6 @@ input_data <- read.table(file = input_file, header = TRUE, sep = "\t") %>%
   remove_rownames() %>% 
   column_to_rownames(var = "ID") %>% 
   as.matrix()
-#pipeline_files <- list.files(pattern = paste(dataset_id, '__.*__de\\.', sep=''))
 
 network <- get_progeny() %>% dplyr::rename(mor=weight)
 
@@ -21,15 +20,3 @@ res_decouple <- decouple(input_data,
                          minsize=0)
 output_file <- paste(sub('__decouplerinput.tsv', '', input_file), 'decoupleroutput.tsv', sep='__')
 write.table(res_decouple, output_file, sep='\t', quote=FALSE, row.names=FALSE)
-
-# Transform to matrix
-mat_consensus <- res_decouple %>%
-  filter(statistic=='consensus') %>%
-  pivot_wider_profile(id_cols = source, names_from = condition, 
-                      values_from = score) %>%
-  as.matrix()
-
-pheatmap(mat_consensus, cluster_rows = F, cluster_cols = F, cellwidth = 20, cellheight = 10)
-
-
-
