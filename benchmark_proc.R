f107fdb07f73234344029216bc91a8edd588aadc
vicpaton <victor.paton@outlook.com>
2023-11-29 13:24:28 +0100

    processing for flop format

--- benchmark_proc.R
+++ benchmark_proc.R
@@ -2,54 +2,65 @@ library(tidyverse)
 
 dir.create('benchmark_data', showWarnings = FALSE)
 
-main_dataset <- 'Simonson2023'
+main_datasets <- c('Simonson2023', 'Armute2023', 'Chaffin2022', 'Koenig2022', 'Kuppe2022', 'Reichart2022')
 
-files_list <- list.files(path = './benchmark', pattern=paste0(main_dataset, '.*\\.csv$'), recursive=TRUE, full.names = TRUE)
+list('Simonson2023' = c('heart_failure', 'sex', 'age'),
+'Armute2023' = c('condition', 'heart_failure'),
+'Chaffin2023' = c('heart_failure', 'sex', 'age'),
+'Koenig2022' = c('heart_failure'),
+'Kuppe2022' = c('sex', 'patient_group'),
+'')
 
-old_metadata <- files_list %>% 
-    grep(pattern='coldata', ., value = TRUE) %>% 
-    read_csv() %>% 
-    .[,-1]
-counts <- files_list %>% grep(pattern='pbulk', ., value = TRUE) %>% read_csv() %>% column_to_rownames('...1') %>% t(.) %>% as.data.frame(.) %>% rownames_to_column('gene_symbol') %>% as_tibble()
-# count number of columns -1
-length(counts) - 1
-old_metadata %>% 
-    nrow()
 
-cell_types <- old_metadata %>% 
-    pull(cell_type) %>% 
-    unique()
 
-for(cell_type in cell_types){
-    metadata <- old_metadata %>% 
-        mutate(group = case_when(
-            cell_type == !!cell_type ~ !!cell_type,
-            cell_type != !!cell_type ~ paste0('No', !!cell_type)
-        )) %>% 
-        mutate(sample_id = paste0(sample_id, '_', cell_type)) %>%
-        select(sample_id, group, disease, sex, age) %>%
-        dplyr::rename('sample_ID' = 'sample_id')
+for(main_dataset in main_datasets){
 
-    contrasts <- tibble(
-        group1 = cell_type,
-        group2 = paste0('No', cell_type)
-    )
+    files_list <- list.files(path = './benchmark', pattern=paste0(main_dataset, '.*\\.csv$'), recursive=TRUE, full.names = TRUE)
 
-    # file output
+    old_metadata <- files_list %>% 
+        grep(pattern='coldata', ., value = TRUE) %>% 
+        read_csv() %>% 
+        .[,-1]
+    counts <- files_list %>% grep(pattern='pbulk', ., value = TRUE) %>% read_csv() %>% column_to_rownames('...1') %>% t(.) %>% as.data.frame(.) %>% rownames_to_column('gene_symbol') %>% as_tibble()
+    # count number of columns -1
+    length(counts) - 1
+    old_metadata %>% 
+        nrow()
 
-    dataset_dir = paste0('benchmark_data/', main_dataset, '_', cell_type, '/')
+    cell_types <- old_metadata %>% 
+        pull(cell_type) %>% 
+        unique()
 
-    dir.create(dataset_dir, showWarnings = FALSE)
+    for(cell_type in cell_types){
+        metadata <- old_metadata %>% 
+            mutate(group = case_when(
+                cell_type == !!cell_type ~ !!cell_type,
+                cell_type != !!cell_type ~ paste0('No', !!cell_type)
+            )) %>% 
+            select(colname, group, heart_failure, sex, age) %>%
+            dplyr::rename('sample_ID' = 'colname')
 
-    counts %>% 
-        write_tsv(paste0(dataset_dir, main_dataset, '_', cell_type, '__countdata.tsv'))
+        contrasts <- tibble(
+            group1 = cell_type,
+            group2 = paste0('No', cell_type)
+        )
 
-    metadata %>%
-        write_tsv(paste0(dataset_dir, main_dataset, '_', cell_type, '__metadata.tsv'))
+        # file output
 
-    contrasts %>%
-        write_tsv(paste0(dataset_dir, main_dataset, '_', cell_type, '__contrast.tsv'))
+        dataset_dir = paste0('benchmark_data/', main_dataset, '_', cell_type, '/')
 
-    contrasts
+        dir.create(dataset_dir, showWarnings = FALSE)
 
+        counts %>% 
+            write_tsv(paste0(dataset_dir, main_dataset, '_', cell_type, '__countdata.tsv'))
+
+        metadata %>%
+            write_tsv(paste0(dataset_dir, main_dataset, '_', cell_type, '__metadata.tsv'))
+
+        contrasts %>%
+            write_tsv(paste0(dataset_dir, main_dataset, '_', cell_type, '__contrast.tsv'))
+
+        contrasts
+
+    }
 }
