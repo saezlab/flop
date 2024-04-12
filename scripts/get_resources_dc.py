3818ab11602ba7715c2cf93e98c0aef6ed43f677
vicpaton <victor.paton@outlook.com>
2024-04-12 15:32:41 +0200

    removed dorothea

--- scripts/get_resources_dc.py
+++ scripts/get_resources_dc.py
@@ -3,14 +3,12 @@ import decoupler as dc
 
 # Gets the three Prior Knowledge sources from the OmniPath database via decoupleR and outputs them as tsv files. 
 progeny = dc.get_progeny(organism='human')
-# dorothea = dc.get_dorothea(organism='human')
 msigdb = dc.get_resource('MSigDB', organism='human')
 collectri = dc.get_collectri(organism='human', split_complexes=False)
 
 msigdb_hallmarks = msigdb.loc[msigdb['collection'] == 'hallmark'].rename(columns={'genesymbol':'target', 'geneset':'source'}).drop_duplicates()
 
 progeny.to_csv('progeny__source.tsv', sep='\t', index=False)
-# dorothea.to_csv('dorothea__source.tsv', sep='\t', index=False)
 collectri.to_csv('collectri__source.tsv', sep='\t', index=False)
 msigdb_hallmarks.to_csv('msigdb_hallmarks__source.tsv', sep='\t', index=False)
 
